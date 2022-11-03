e5b5fe5c24be98414a637316e5e5b21f6bf537e6
Juan ismael Camacho cervantes <2000084883@hexaware.com>
2022-11-03 11:29:47 -0600

    added one line.

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Juan Ismael Camacho Cervantes</code>
+          <code>Juan Ismael Camacho Cervantes - CICD Course</code>
         </p>
         <a
           className="App-link"
