086c005a334b38a521648b6f84211fca0c53cf12
Juan ismael Camacho cervantes <2000084883@hexaware.com>
2022-11-04 09:25:09 -0600

    Corrected line 17 yml file.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Juan Ismael Camacho Cervantes - CICD Course</code>
+          <code>Juan Ismael Camacho Cervantes - CICD Course - Github Pages</code>
         </p>
         <a
           className="App-link"
