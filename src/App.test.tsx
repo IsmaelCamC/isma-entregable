939aff4d27d7f21b7d13fe82113e49be40b1fff2
Juan ismael Camacho cervantes <2000084883@hexaware.com>
2022-11-04 10:29:32 -0600

    Added some corrections.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Go to QA/i);
+  expect(linkElement).toBeInTheDocument();
+  const linkElement2 = screen.getByText(/Go to Production/i);
   expect(linkElement).toBeInTheDocument();
 });
